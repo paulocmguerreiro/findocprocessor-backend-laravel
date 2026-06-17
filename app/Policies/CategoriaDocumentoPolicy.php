689779f6a8e8034c1342747ecde64e16ee26aa15
Paulo Guerreiro <paulocmguerreiro@gmail.com>
2026-06-17 12:56:32 +0100

    feat(auth): criar CategoriaDocumentoPolicy com acesso aberto — Issue #25

---
+++ app/Policies/CategoriaDocumentoPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+final class CategoriaDocumentoPolicy
+{
+    public function viewAny(): bool
+    {
+        return true;
+    }
+
+    public function view(): bool
+    {
+        return true;
+    }
+
+    public function create(): bool
+    {
+        return true;
+    }
+
+    public function update(): bool
+    {
+        return true;
+    }
+
+    public function delete(): bool
+    {
+        return true;
+    }
+}
