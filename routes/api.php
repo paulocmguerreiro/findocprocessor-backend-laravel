d97907787406aa37d452cd78f2e510c2143012c1
Paulo Guerreiro <paulocmguerreiro@gmail.com>
2026-06-19 12:00:04 +0100

    feat(entidade): rotas apiResource + empresa-mae

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 use App\Features\CategoriaDocumento\CategoriaDocumentoController;
+use App\Features\Entidade\EntidadeController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categorias-documento', CategoriaDocumentoController::class);
+
+Route::apiResource('entidades', EntidadeController::class);
+Route::patch('entidades/{entidade}/empresa-mae', [EntidadeController::class, 'converterEmEmpresaMae']);
