f059307c71fa256ca21f2a32736a679502225434
Paulo Guerreiro <paulocmguerreiro@gmail.com>
2026-06-19 11:57:37 +0100

    feat(entidade): ConverterEmEmpresaMaeAction

---
+++ app/Features/Entidade/EmpresaMae/ConverterEmEmpresaMaeAction.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Features\Entidade\EmpresaMae;
+
+use App\Models\Entidade;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+
+final readonly class ConverterEmEmpresaMaeAction
+{
+    public function __construct(
+        private RegraUnicidadeEmpresaMae $regraUnicidade,
+    ) {}
+
+    /**
+     * @throws ModelNotFoundException<Entidade>
+     * @throws AuthorizationException
+     * @throws \Throwable
+     */
+    public function handle(Entidade|string $idEntidade): Entidade
+    {
+        /** @var Entidade $entidade */
+        $entidade = is_string($idEntidade)
+            ? Entidade::findOrFail($idEntidade)
+            : $idEntidade;
+
+        Gate::authorize('update', $entidade);
+
+        return DB::transaction(function () use ($entidade): Entidade {
+            $this->regraUnicidade->handle(true);
+
+            $entidade->update([
+                'e_empresa_aplicacao' => true,
+                'e_cliente' => true,
+                'e_fornecedor' => true,
+            ]);
+
+            $entidade->refresh();
+
+            return $entidade;
+        });
+    }
+}
